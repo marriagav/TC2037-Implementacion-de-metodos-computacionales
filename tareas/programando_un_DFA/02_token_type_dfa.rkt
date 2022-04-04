8db86a7257ebef4ff261d12cf5c7c9031b080e25
Miguel Arriaga <marriagav3@gmail.com>
2022-04-04 12:25:09 -0500

    added parenthesis functionality

--- tareas/programando_un_DFA/02_token_type_dfa.rkt
+++ tareas/programando_un_DFA/02_token_type_dfa.rkt
@@ -5,8 +5,9 @@
     Miguel Arriaga
 
     Examples:
-    (automaton-2 (dfa-str 'start '(int var float comment) delta-arithmetic-1) "a = 32.4 *-8.6 - b/      6.1E-8")
-    (automaton-2 (dfa-str 'start '(int var float comment) delta-arithmetic-1) "d = a ^ b+5 // Esto es un comentario")
+    (arithmetic-lexer "a = 32.4 *-8.6 - b/      6.1E-8")
+    (arithmetic-lexer "d = a ^ b+5 // Esto es un comentario")
+    (arithmetic-lexer "d = (a ^ b)+5+5.0e4 // Esto es un comentario")
 |#
 
 #lang racket
@@ -19,7 +20,7 @@
 
 (define (arithmetic-lexer string)
     "Main function that calls the automaton and lexes a string"
-    (automaton-2 (dfa-str 'start '(int var float comment exp) '(o_sp n_sp) delta-arithmetic-1) string))
+    (automaton-2 (dfa-str 'start '(int var float comment exp par_close) '(o_sp n_sp) delta-arithmetic-1) string))
 
 (define (automaton-2 dfa input-string)
     "Evaluate a string to validate or not according to a DFA.
@@ -73,6 +74,12 @@
 (define (exp? char)
     (member char '(#\e #\E)))
 
+(define (par_open? char)
+    (member char (list(integer->char 40))))
+
+(define (par_close? char)
+    (member char (list(integer->char 41))))
+
 (define (delta-arithmetic-1 state character)
     "Transition function for an automaton that accepts basic arithmetic operations"
     (case state
@@ -81,6 +88,7 @@
                     [(or (char-alphabetic? character) (eq? character #\_)) (values #f 'var)]
                     [(sign? character) (values #f 'n_sign)]
                     [(space? character) (values #f 'o_sp)]
+                    [(par_open? character) (values #f 'par_open)]
                     [else (values #f 'fail)])]
         ['n_sign (cond
                     [(char-numeric? character) (values #f 'int)]
@@ -92,6 +100,8 @@
                     [(space? character) (values 'int 'n_sp)]
                     [(dot? character) (values #f 'float)]
                     [(exp? character) (values #f 'exp_pow)]
+                    [(par_open? character) (values 'int 'par_open)]
+                    [(par_close? character) (values 'int 'par_close)]
                     [else (values #f 'fail)])]
         ['float (cond
                     [(char-numeric? character) (values #f 'float)]
@@ -99,6 +109,8 @@
                     [(division? character) (values 'float 'div)]
                     [(space? character) (values 'float 'n_sp)]
                     [(exp? character) (values #f 'exp_pow)]
+                    [(par_open? character) (values 'float 'par_open)]
+                    [(par_close? character) (values 'float 'par_close)]
                     [else (values #f 'fail)])]
         ['exp_pow (cond
                     [(char-numeric? character) (values #f 'exp)]
@@ -109,6 +121,8 @@
                     [(operator? character) (values 'exp 'op)]
                     [(division? character) (values 'exp 'div)]
                     [(space? character) (values 'exp 'n_sp)]
+                    [(par_open? character) (values 'exp 'par_open)]
+                    [(par_close? character) (values 'exp 'par_close)]
                     [else (values #f 'fail)])]
         ['var (cond
                     [(or (char-alphabetic? character) (eq? character #\_)) (values #f 'var)]
@@ -116,35 +130,57 @@
                     [(operator? character) (values 'var 'op)]
                     [(division? character) (values 'var 'div)]
                     [(space? character) (values 'var 'n_sp)]
+                    [(par_open? character) (values 'var 'par_open)]
+                    [(par_close? character) (values 'var 'par_close)]
                     [else (values #f 'fail)])]
         ['op (cond
                     [(char-numeric? character) (values 'op 'int)]
                     [(sign? character) (values 'op 'n_sign)]
                     [(or (char-alphabetic? character) (eq? character #\_)) (values 'op 'var)]
                     [(space? character) (values 'op 'o_sp)]
-                    ; [(dot? character) (values #f 'float)]
+                    [(par_open? character) (values 'op 'par_open)]
+                    [(par_close? character) (values 'op 'par_close)]
                     [else (values #f 'fail)])]
         ['div (cond
                     [(char-numeric? character) (values 'op 'int)]
                     [(sign? character) (values 'op 'n_sign)]
                     [(or (char-alphabetic? character) (eq? character #\_)) (values 'op 'var)]
                     [(space? character) (values 'op 'o_sp)]
-                    ; [(dot? character) (values #f 'float)]
                     [(division? character) (values #f 'comment)]
+                    [(par_open? character) (values 'op 'par_open)]
+                    [(par_close? character) (values 'op 'par_close)]
+                    [else (values #f 'fail)])]
+        ['par_open (cond
+                    [(char-numeric? character) (values 'par_open 'int)]
+                    [(sign? character) (values 'par_open 'n_sign)]
+                    [(or (char-alphabetic? character) (eq? character #\_)) (values 'par_open 'var)]
+                    [(par_open? character) (values 'par_open 'par_open)]
+                    [(par_close? character) (values 'par_open 'par_close)]
+                    [(space? character) (values 'par_open 'o_sp)]
+                    [else (values #f 'fail)])]
+        ['par_close (cond
+                    [(operator? character) (values 'par_close 'op)]
+                    [(par_open? character) (values 'par_close 'par_open)]
+                    [(par_close? character) (values 'par_close 'par_close)]
+                    [(space? character) (values 'par_close 'n_sp)]
+                    [(division? character) (values 'par_close 'div)]
                     [else (values #f 'fail)])]
         ['comment (cond
                     [else (values #f 'comment)])]
         ['o_sp (cond
                     [(char-numeric? character) (values #f 'int)]
-                    ; [(dot? character) (values #f 'float)]
                     [(sign? character) (values #f 'n_sign)]
                     [(space? character) (values #f 'o_sp)]
                     [(or (char-alphabetic? character) (eq? character #\_)) (values #f 'var)]
+                    [(par_open? character) (values #f 'par_open)]
+                    [(par_close? character) (values #f 'par_close)]
                     [else (values #f 'fail)])]
         ['n_sp (cond
                     [(operator? character) (values #f 'op)]
                     [(division? character) (values #f 'div)]
                     [(space? character) (values #f 'n_sp)]
+                    [(par_open? character) (values #f 'par_open)]
+                    [(par_close? character) (values #f 'par_close)]
                     [else (values #f 'fail)])]
         ['fail (values #f 'fail)]
     )
