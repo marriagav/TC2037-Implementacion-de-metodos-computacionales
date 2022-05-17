2361c204bb580fcee6df006924ccd855613da502
Pablo Rocha <pablorochatmm@gmail.com>
2022-05-17 11:03:57 -0500

    list

--- actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/lib/resaltador_de_sintaxis.ex
+++ actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/lib/resaltador_de_sintaxis.ex
@@ -12,21 +12,42 @@ defmodule ResaltadorDeSintaxis do
       :world
 
   """
-  def get_emails(in_filename,out_file) do
+  def json_praser(in_filename,out_file) do
     text =
       in_filename
       |>File.stream!()
-      |>Enum.map(&email_from_line/1)
-      |>Io.inspects
+      |>Enum.map(&eachline/1)
       |>Enum.filter(fn x -> x !== nil end)
       |>Enum.map(&hd/1)
       |> Enum.join("\n")
     File.write(out_file,text)
   end
 
-  defp email_from_line(line) do
-    Regex.run(~r|\w+(?:\.\w+)*@\w+(?:\.\w+)*\.\w{2,4}|,line)
+  defp eachline(line) do
+    withoutabs = deleteWS(line)
+    # IO.inspect withoutabs
+    # Regex.run(~r|^[{}:,""]|,withoutabs)
+    # Regex.run(~r|\t|,line)
+    # Regex.run(~r|^[{}:,""]|,line)
+    goThroughLine(line,[])
   end
+  defp goThroughLine("",tokens), do: Enum.reverse(tokens)
+  defp goThroughLine(line,tokens) do
+    IO.inspect tokens
+    if Regex.match?(~r|^\s|,line) do
+      found = Regex.run(~r|^\s|,line)
+      goThroughLine(deleteFromString(line,found),[found|tokens])
+    end
+    if Regex.match?(~r|^[{}:,""]|,line) do
+      found = Regex.run(~r|^[{}:,""]|,line)
+      IO.puts "AQUI"
+      IO.inspect found
+      goThroughLine(deleteFromString(line,found),[found|tokens])
+    end
+  end
+
+  defp deleteWS(string) when is_binary(string) do string |> String.replace(~r|\s|, "") end
+  defp deleteFromString(string,rp) when is_binary(string) do string |> String.replace(rp, "") end
 end
 
-IO.inspect ResaltadorDeSintaxis.get_emails("lloren_emails.txt","num.txt")
+ResaltadorDeSintaxis.json_praser("json_test.txt","json.html")
