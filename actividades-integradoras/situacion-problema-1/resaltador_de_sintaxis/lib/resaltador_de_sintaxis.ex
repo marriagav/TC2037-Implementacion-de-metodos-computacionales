78f38ad148621ddf1c61adae24c94bcb537ca637
Miguel Arriaga <marriagav3@gmail.com>
2022-05-17 12:22:41 -0500

    bugfix

--- actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/lib/resaltador_de_sintaxis.ex
+++ actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/lib/resaltador_de_sintaxis.ex
@@ -18,15 +18,12 @@ defmodule ResaltadorDeSintaxis do
       |>File.stream!()
       |>Enum.map(&eachline/1)
       |>Enum.filter(fn x -> x !== nil end)
-      # |>Enum.map(&hd/1)
       |> Enum.join("")
     File.write(out_file,text)
   end
 
   defp eachline(line) do
     a = goThroughLine(line,[])
-    # IO.puts "FINAL LINE"
-    # IO.inspect a
     a
   end
   defp goThroughLine("",tokens), do: Enum.reverse(tokens)
@@ -34,12 +31,13 @@ defmodule ResaltadorDeSintaxis do
     cond do
       Regex.match?(~r|^[\s]+|,line)  ->
         found = Regex.run(~r|^[\s]+|,line)
-        IO.inspect found
         goThroughLine(deleteFromString(line,found),[found|tokens])
       Regex.match?(~r|^"[a-zA-Z-0-9]+":|,line) ->
         found = Regex.run(~r|^"[a-zA-Z-0-9]+":|,line)
-        html = "<spam class=\"object-key\">#{found}</spam>"
-        goThroughLine(deleteFromString(line,found),[html|tokens])
+        withoutPunct=hd(found)
+        withoutPunct=deleteFromString(withoutPunct,":")
+        html = "<spam class=\"object-key\">#{withoutPunct}</spam>"
+        goThroughLine(deleteFromString(line,[withoutPunct]),[html|tokens])
       Regex.match?(~r|^[{}:,]|,line) ->
         found = Regex.run(~r|^[{}:,]|,line)
         html = "<spam class=\"punctuation\">#{found}</spam>"
@@ -49,7 +47,6 @@ defmodule ResaltadorDeSintaxis do
     end
   end
 
-  defp deleteWS(string) when is_binary(string) do string |> String.replace(~r|\s|, "") end
   defp deleteFromString(string,rp) when is_binary(string) do string |> String.replace(rp, "") end
 end
 
