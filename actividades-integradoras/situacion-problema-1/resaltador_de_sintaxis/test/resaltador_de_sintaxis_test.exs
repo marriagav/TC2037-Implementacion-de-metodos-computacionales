3b0e63417584849e12e55d90fa239810a54ed2a1
Pablo Rocha <pablorochatmm@gmail.com>
2022-05-17 09:53:06 -0500

    la

---
+++ actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/test/resaltador_de_sintaxis_test.exs
@@ -0,0 +1,8 @@
+defmodule ResaltadorDeSintaxisTest do
+  use ExUnit.Case
+  doctest ResaltadorDeSintaxis
+
+  # test "greets the world" do
+  #   assert ResaltadorDeSintaxis.hello() == :world
+  # end
+end
