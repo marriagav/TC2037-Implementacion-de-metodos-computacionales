c400afd2501c121fd067625213d4e96bd9efae28
Pablo Rocha <pablorochatmm@gmail.com>
2022-05-20 10:39:56 -0500

    separate regex into variables

--- actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/lib/resaltador_de_sintaxis.ex
+++ actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/lib/resaltador_de_sintaxis.ex
@@ -37,36 +37,42 @@ defmodule ResaltadorDeSintaxis do
   #Function that identifies the tolkens of a line and returns them in html format
   defp goThroughLine("",tokens), do: Enum.reverse(tokens)
   defp goThroughLine(line,tokens) do
+    regex_space = ~r|^[\s]+|
+    regex_keys = ~r|^"[^,]+?(?=["])"[\s]*:|
+    regex_string = ~r|^".*?(?=["])"|
+    regex_numbers = ~r|^[-\|+]?[\d.]+(?:[e\|E]-?\d+)?|
+    regex_reserved = ~r|^\btrue\b\|^\bnull\b\|^\bfalse\b|
+    regex_punctuation = ~r|^[{}:,\[\]]|
     cond do
       #Look for spaces and trim
-      Regex.match?(~r|^[\s]+|,line)  ->
-        found = Regex.run(~r|^[\s]+|,line)
+      Regex.match?(regex_space,line)  ->
+        found = Regex.run(regex_space,line)
         goThroughLine(deleteWS(line),[found|tokens])
       #Regex for keys
-      Regex.match?(~r|^"[^,]+?(?=["])"[\s]*:|,line) ->
-        found = Regex.run(~r|^"[^,]+?(?=["])"[\s]*:|,line)
+      Regex.match?(regex_keys,line) ->
+        found = Regex.run(regex_keys,line)
         withoutPunct=hd(found)
         withoutPunct=deletePunctFromKey(withoutPunct)
         html = "<spam class=\"object-key\">#{withoutPunct}</spam>"
         goThroughLine(deleteFromString(line,withoutPunct),[html|tokens])
       #Regex for strings
-      Regex.match?(~r|^".*?(?=["])"|,line) ->
-        found = Regex.run(~r|^".*?(?=["])"|,line)
+      Regex.match?(regex_string,line) ->
+        found = Regex.run(regex_string,line)
         html = "<spam class=\"string\">#{found}</spam>"
         goThroughLine(deleteFromString(line,hd(found)),[html|tokens])
       #Regex for numbers
-      Regex.match?(~r|^[-\|+]?[\d.]+(?:[e\|E]-?\d+)?|,line) ->
-        found = Regex.run(~r|^[-\|+]?[\d.]+(?:[e\|E]-?\d+)?|,line)
+      Regex.match?(regex_numbers,line) ->
+        found = Regex.run(regex_numbers,line)
         html = "<spam class=\"number\">#{found}</spam>"
         goThroughLine(deleteFromString(line,hd(found)),[html|tokens])
       #Regex for reserved words
-      Regex.match?(~r|^\btrue\b\|^\bnull\b\|^\bfalse\b|,line) ->
-        found = Regex.run(~r|^\btrue\b\|^\bnull\b\|^\bfalse\b|,line)
+      Regex.match?(regex_reserved,line) ->
+        found = Regex.run(regex_reserved,line)
         html = "<spam class=\"reserved-word\">#{found}</spam>"
         goThroughLine(deleteFromString(line,hd(found)),[html|tokens])
       #Regex for puntuation
-      Regex.match?(~r|^[{}:,\[\]]|,line) ->
-        found = Regex.run(~r|^[{}:,\[\]]*|,line)
+      Regex.match?(regex_punctuation,line) ->
+        found = Regex.run(regex_punctuation,line)
         html = "<spam class=\"punctuation\">#{found}</spam>"
         goThroughLine(deleteFromString(line,hd(found)),[html|tokens])
       #Base case
