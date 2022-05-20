b6189cdfd11aa97c60b116ccdfba31841824b126
Miguel Arriaga <marriagav3@gmail.com>
2022-05-20 10:10:45 -0500

    added more example files

--- actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/test/resaltador_de_sintaxis_test.exs
+++ actividades-integradoras/situacion-problema-1/resaltador_de_sintaxis/test/resaltador_de_sintaxis_test.exs
@@ -11,5 +11,11 @@ defmodule ResaltadorDeSintaxisTest do
     ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_4.json","./lib/html_output_files/html_4.html","./lib/template_page.html")
     ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_5.json","./lib/html_output_files/html_5.html","./lib/template_page.html")
     ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_6.json","./lib/html_output_files/html_6.html","./lib/template_page.html")
+    ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_7.json","./lib/html_output_files/html_7.html","./lib/template_page.html")
+    ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_8.json","./lib/html_output_files/html_8.html","./lib/template_page.html")
+    ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_9.json","./lib/html_output_files/html_9.html","./lib/template_page.html")
+    ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_10.json","./lib/html_output_files/html_10.html","./lib/template_page.html")
+    ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_11.json","./lib/html_output_files/html_11.html","./lib/template_page.html")
+    ResaltadorDeSintaxis.json_praser("./lib/test_json_files/example_12.json","./lib/html_output_files/html_12.html","./lib/template_page.html")
   end
 end
